7f88439ed576ea7f6749dedf08ef2caf42c97eab
Richard.AmoakoAgyei <richard.amoakoagyei@target.com>
2017-12-13 16:40:46 -0600

    bs-datatable

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+
+export default {
+    input: 'dist/index.js',
+    output: {
+      file: 'dist/bundles/datatable.umd.js',
+      format: 'umd',
+      name: 'ng.datatable'
+    },
+    sourceMap: false,
+    globals: {
+      '@angular/core': 'ng.core',
+      'rxjs/Observable': 'Rx',
+      'rxjs/ReplaySubject': 'Rx',
+      'rxjs/add/operator/map': 'Rx.Observable.prototype',
+      'rxjs/add/operator/mergeMap': 'Rx.Observable.prototype',
+      'rxjs/add/observable/fromEvent': 'Rx.Observable',
+      'rxjs/add/observable/of': 'Rx.Observable'
+    }
+  }
